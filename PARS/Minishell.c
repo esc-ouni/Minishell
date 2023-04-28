71f2bc00dbe051e231bf14161fdac9dcc2f24fb9
Ibrahim Douni <idouni@e1r12p1.1337.ma>
2023-04-28 10:11:32 +0100

    Minishell

--- PARS/Minishell.c
+++ PARS/Minishell.c
@@ -14,28 +14,11 @@
 
 int main() 
 {
-    // t_list  *n;
-    // char    **s;
-    // s = malloc(8 * 4);
-    // s[0] = malloc(8 * 10);
-    // s[1] = malloc(8 * 10);
-    // s[2] = malloc(8 * 10);
-    // s[3] = NULL;
-    // strcpy(s[0], "grep");
-    // strcpy(s[1], "\"me");
-    // strcpy(s[2], "hello\"");
-    start();
+    // start();
     while (1)
     {
         prompt();
         after_parse(parser());
-        // execve("/usr/bin/grep", s,NULL);
-        // system(n->cmd);
     }
-    // free(s[0]);
-    // free(s[1]);
-    // free(s[2]);
-    // free(s[3]);
-    // free(s);
     return 0;
 }
