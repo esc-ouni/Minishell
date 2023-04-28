515ced18c0337bb9d984f1c712ad3f75fc723d0c
Mohamed-Taha Samhaoui <msamhaou@e2r4p1.1337.ma>
2023-04-28 01:55:59 +0100

    multiple

--- EXEC/main.c
+++ EXEC/main.c
@@ -6,18 +6,51 @@
 /*   By: msamhaou <msamhaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 18:35:34 by msamhaou          #+#    #+#             */
-/*   Updated: 2023/04/25 15:43:29 by msamhaou         ###   ########.fr       */
+/*   Updated: 2023/04/28 00:53:44 by msamhaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_cmd	**init()
+
+int	siglol = 0;
+
+void	files_init(t_file **file)
+{
+	int	num_files = 1;
+	int	i = 0;
+	t_file	*head = (*file);
+
+	while (i < num_files)
+	{
+		if (!(*file))
+			(*file) = (t_file *)malloc(sizeof(t_file));
+		(*file)->next = (t_file *)malloc(sizeof(t_file));
+		(*file) = (*file)->next;
+		i++;
+	}
+	(*file)->next = NULL;
+	*file = head;
+
+	/*file 1*/
+	(*file)->filename = "out1.txt";
+	/*file 2*/
+	(*file)->next->filename = "out2.txt";
+
+
+	//return(head);
+}
+
+t_cmd	**init(char	**env)
 {
 	t_cmd **lol;
 	int	stdinit;
+	char	**myenv;
 	enum	flags;
+	t_file	*files ;
+	files_init(&files);
 
+	myenv = ft_set_env(env);
 	stdinit = dup(0);
 
 	lol = (t_cmd **)malloc(sizeof(t_cmd *) * 3);
@@ -26,38 +59,54 @@ t_cmd	**init()
 		lol[i++] = (t_cmd *)malloc(sizeof(t_cmd));
 	lol[i] = NULL;
 	//cmd 000
-	lol[0]->cmd = ft_split("cd ../PARS", ' ');
-	lol[0]->cmd_path = "/usr/bin/cat";
+	lol[0]->cmd = ft_split("pwd", ' ');
+	lol[0]->env = myenv;
+	//lol[0]->cmd_path = "/usr/bin/cat";
 	lol[0]->cmd_flag = BUILT;
-	lol[0]->input_file = "nnn.c";
+	lol[0]->builtflag = PWD;
+	lol[0]->input_file = NULL;
+	lol[0]->output_file = NULL;
 	lol[0]->cmd_fdin = 0;
 	lol[0]->inputed = 0;
 	lol[0]->first_cmd = 1;
 	lol[0]->last_cmd = 1;
 	lol[0]->init_stdin = stdinit;
-
-	//cmd 111
-	// lol[1]->cmd = ft_split("wc -l", ' ');
-	// lol[1]->cmd_path = "/usr/bin/wc";
-	// lol[1]->cmd_flag = SYS;
-	// lol[1]->input_file = NULL;
-	// lol[1]->output_file = NULL;
-	// lol[1]->first_cmd = 0;
-	// lol[1]->outputed = 0;
-	// lol[1]->last_cmd = 1;
-	// lol[1]->init_stdin = stdinit;
-	// lol[1]->next = NULL;
-
-	// //cmd 222
-	// lol[2]->cmd = ft_split("wc -l", ' ');
-	// lol[2]->cmd_path = "/usr/bin/wc";
-	// lol[2]->input_file = NULL;
-	// lol[2]->first_cmd = 0;
-	// lol[2]->outputed = 0;
-	// lol[2]->last_cmd = 1;
-	// lol[2]->init_stdin = stdinit;
-	// lol[2]->next = NULL;
+	lol[0]->out_files = files;
 	return lol;
+}
+
+int	ft_builtin(t_cmd *lol)
+{
+	if (lol->builtflag == ECHO)
+		ft_echo(lol);
+	else if (lol->builtflag == CD)
+		ft_cd(lol);
+	else if (lol->builtflag == PWD)
+		ft_pwd();
+	else if (lol->builtflag == EXPORT)
+		ft_export(lol->env, lol->cmd[1]);
+	// else if (lol->builtflag == UNSET)
+		
+	// else if (lol->builtflag == ENV)
+		
+	// else if (lol->builtflag == EXIT)
+		
+	return(0);
+}
+
+
+int	ft_open_files(t_cmd *lol)
+{
+	t_file	*files;
+
+	files = lol->out_files;
+	while (files->next)
+	{
+		open(files->filename, O_CREAT | O_WRONLY , 0664);
+		files = files->next;
+	}
+	lol->cmd_fdout = open(files->filename, O_CREAT | O_WRONLY , 0664);
+	return (0);
 }
 
 int	ft_fork(t_cmd *lol, char **env)
@@ -79,22 +128,22 @@ int	ft_fork(t_cmd *lol, char **env)
 	if (pid == 0)
 	{
 		close(fd[0]);
-		if (lol->output_file)
+		if (lol->out_files)
 		{
-			lol->cmd_fdout = open(lol->output_file, O_CREAT | O_WRONLY, 0664);
+			ft_open_files(lol);
 			dup2(lol->cmd_fdout, STDOUT_FILENO);
 		}
 		else
 			dup2(fd[1], STDOUT_FILENO);
 
 		if (lol->cmd_flag == BUILT)
-			{
-				ft_cd(lol);
-				exit (0);
-			}
+		{
+			ft_builtin(lol);
+			exit (0);
+		}
 		else if (lol->cmd_flag == SYS)
-			{if (execve(lol->cmd_path, lol->cmd, env) < 0)
-				exit(1);}
+			if (execve(lol->cmd_path, lol->cmd, env) < 0)
+				return(perror(""), 1);
 	}
 	else
 	{
@@ -107,16 +156,11 @@ int	ft_fork(t_cmd *lol, char **env)
 		{
 			char buff[5];
 			while (read(fd[0], buff, 5) > 0)
-				write(1, buff, 5);
+				write(STDOUT_FILENO, buff, 5);
 		}
 		close (fd[0]);
 	}
 	return 0;
-}
-
-int	builting_cmd(t_cmd *lol, char **env)
-{
-	return(0);
 }
 
 void	ft_prompt()
@@ -159,9 +203,33 @@ char	**ft_set_env(char **env)
 	return(res);
 }
 
+/********************SIGNALES********************/
+void	sig_handle(int sig)
+{
+	if (sig == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);/*should be one after history setup*/
+		rl_redisplay();
+		ft_prompt();
+		//siglol = 1;
+	}
+	else if(sig == SIGQUIT)
+	{
+		ft_prompt();
+		rl_redisplay();
+		write(1, "exit\n", 5);
+		exit(0);
+	}
+}
+/********************SIGNALES********************/
+
 int main(int ac, char **av, char **env)
 {
-	char 	*r;
+	(void)av;
+	(void)ac;
+	char	*r;
 	int		tmp_fd_in;
 	int		tmp_fd_out;
 	t_cmd	**lol;
@@ -170,13 +238,16 @@ int main(int ac, char **av, char **env)
 	i = 0;
 	tmp_fd_in = dup(STDIN_FILENO);
 	tmp_fd_out = dup (STDOUT_FILENO);
-	lol = init();
+	lol = init(env);
 	while (1)
 	{
 		i = 0;
+		siglol = 0;
 		dup2(tmp_fd_in, 0);
 		dup2(tmp_fd_out, 1);
 		ft_prompt();
+		signal(SIGINT, sig_handle);
+		signal(SIGQUIT, sig_handle);
 		r = readline("");
 		while (i < 1)
 			ft_fork(lol[i++], env);
--- EXEC/minishell.h
+++ EXEC/minishell.h
@@ -2,43 +2,60 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include <unistd.h>
+# include <stdio.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/readline.h>
 # include <stdlib.h>
 # include <sys/types.h>
 # include <dirent.h>
-# include <readline/readline.h>
-# include <readline/history.h>
 # include "libft/libft.h"
 
-enum    flags{
-    SYS,
-    BUILT
+enum    flags
+{
+	SYS,
+	BUILT
+};
+
+enum    builtin
+{
+	NOT,
+	ECHO,
+	CD,
+	PWD,
+	EXPORT,
+	UNSET,
+	ENV,
+	EXIT
 };
 
 typedef struct s_file
 {
-    char *filename;
-    int infile;
-    int outfile;
-
+	char            *filename;
+	int				o_flags; /*Append  O_TRUNC O_CREAT .... */
+	struct s_file   *next;
 }   t_file;
 
 typedef struct s_cmd
 {
-    char    **cmd;
-    int     cmd_flag;
-    char    *input_file;
-    char    *output_file;
-    char    *cmd_path;
-    int     cmd_pos;
-    int     init_stdin;
-    int     first_cmd;
-    int     last_cmd;
-    int     cmd_fdin;
-    int     inputed;
-    int     outputed;
-    int     cmd_fdout;
-    int     *fd;
-    struct s_cmd *next;
+	char			**env;
+	char			**cmd;
+	int				cmd_flag;
+	char			*input_file;
+	char			*output_file;
+	enum builtin	builtflag;
+	char			*cmd_path;
+	int				cmd_pos;
+	int				init_stdin;
+	int				first_cmd;
+	int				last_cmd;
+	int				cmd_fdin;
+	int				inputed;
+	int				outputed;
+	int				cmd_fdout;
+	int				*fd;
+	t_file			*in_files;
+	t_file			*out_files;
 }   t_cmd;
 
 int	ft_execute(char *arg, char **env);
@@ -49,5 +66,6 @@ int	ft_echo(t_cmd *lol);
 char	**ft_export(char **env, char *str);
 void	ft_free_old_env(char **env);
 void	ft_free_pos(char **strp, int pos);
+char	**ft_set_env(char **env);
 
 #endif
