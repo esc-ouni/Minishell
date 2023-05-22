65c2112ecbeccf276553c78c10e105d3f7c0cb4c
esc-ouni <idouni@student.1337.ma>
2023-05-22 14:39:23 +0100

    readability_in_progress

--- tools_6.c
+++ tools_6.c
@@ -1,59 +1,69 @@
 #include "Minishell.h"
 
-void	allocate_full_cmd(t_collector **collector, char **full_cmd, char	***str)
+char	**allocate_full_cmd(t_collector **collector, char **full_cmd)
 {
+	char	**str;
 	int	 i;
 
 	i = 0;
+	str = NULL;
 	if (full_cmd)
 	{
 		while (full_cmd[i])
 			i++;
-		(*str) = h_malloc(collector, sizeof(char *) * (i + 1), (*str));
+		str = h_malloc(collector, sizeof(char *) * (i + 1), str);
 		i = 0;
 		while (full_cmd[i])
 		{
-			(*str)[i] = ft_mstrdup(collector, full_cmd[i]);
+			str[i] = ft_mstrdup(collector, full_cmd[i]);
 			i++;
 		}
-		(*str)[i] = NULL;
+		str[i] = NULL;
 	}
 	else
 	{
-		(*str) = h_malloc(collector, sizeof(char *), (*str));
-		(*str)[0] = NULL;
+		str = h_malloc(collector, sizeof(char *), str);
+		str[0] = NULL;
 	}
+	return (str);
 }
 
-void 	add_to_cmd(t_collector **collector, t_cmd **head, char **full_cmd, t_file *out_files, t_file *in_files)
+t_file	*allocate_out_files(t_collector **collector, t_file *out_files)
 {
-	int		i;
 	t_file	*out_files2;
-	t_file	*in_files2;
-	t_cmd 	*new_cmd;
-	char	**str;
-	t_cmd	*tmp;
 
-	i = 0;
-	str = NULL;
-	new_cmd = NULL;
-	new_cmd = h_malloc(collector, sizeof(t_cmd), new_cmd);
 	out_files2 = NULL;
-	in_files2 = NULL;
-	allocate_full_cmd(collector, full_cmd, &str);
 	while (out_files)
 	{
 		add_file_file(collector, &out_files2, out_files);
 		out_files = out_files->next;
 	}
+	return (out_files2);
+}
+
+t_file	*allocate_in_files(t_collector **collector, t_file *in_files)
+{
+	t_file	*in_files2;
+
+	in_files2 = NULL;
 	while (in_files)
 	{
 		add_file_file(collector, &in_files2, in_files);
 		in_files = in_files->next;
 	}
-	new_cmd->cmd = str;
-	new_cmd->out_files = out_files2;
-	new_cmd->in_files = in_files2;
+	return (in_files2);
+}
+
+void 	add_to_cmd(t_collector **collector, t_cmd **head, char **full_cmd, t_file *out_files, t_file *in_files)
+{
+	t_cmd 	*new_cmd;
+	t_cmd	*tmp;
+
+	new_cmd = NULL;
+	new_cmd = h_malloc(collector, sizeof(t_cmd), new_cmd);
+	new_cmd->cmd = allocate_full_cmd(collector, full_cmd);
+	new_cmd->out_files = allocate_out_files(collector, out_files);
+	new_cmd->in_files = allocate_in_files(collector, in_files);
 	if (!(*head))
         *head = new_cmd;
     else
