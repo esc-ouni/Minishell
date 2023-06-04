6213ed8102f973d5857927032a2a344cef07dbae
esc-ouni <idouni@student.1337.ma>
2023-06-04 13:58:40 +0100

    REDIRECTIONS_DONE

--- tools_10.c
+++ tools_10.c
@@ -6,7 +6,7 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 17:08:28 by idouni            #+#    #+#             */
-/*   Updated: 2023/05/28 16:14:55 by idouni           ###   ########.fr       */
+/*   Updated: 2023/06/04 13:58:09 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ char	*prompt(void)
 	char	*s;
 
 	rl_on_new_line();
-	s = readline("BAASH >> ");
+	s = readline("BAASH$ ");
 	return (s);
 }
 
