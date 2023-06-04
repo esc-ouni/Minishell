cc9e9cfb290c3b583f094788b813215123c8d117
esc-ouni <idouni@student.1337.ma>
2023-06-04 18:37:29 +0100

    ENHANCED_COLLECTOR

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msamhaou <msamhaou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 11:08:05 by msamhaou          #+#    #+#             */
-/*   Updated: 2023/06/04 16:25:47 by msamhaou         ###   ########.fr       */
+/*   Updated: 2023/06/04 18:35:48 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,7 +83,33 @@ struct s_struct
 	int			cmd_numb;
 };
 
+typedef struct s_ntmp
+{
+    void            *ntmp_addr;
+    struct s_ntmp	*next;
+}   t_ntmp;
 
+typedef struct s_tmp
+{
+    void           	*tmp_addr;
+    struct s_tmp	*next;
+}   t_tmp;
+
+typedef struct s_collector
+{
+    t_tmp				*tmp_cltr;
+    t_ntmp				*ntmp_cltr;
+}   t_collector;
+
+typedef enum e_flag
+{
+	TMP,
+	NTMP,
+	ALL
+}	t_flag;
+
+void	*h_malloc(t_collector **collector, size_t s, void *p, t_flag flag);
+void	ft_collectorclear(t_collector **collector, t_flag flag);
 
 void	ft_set_env_lst(t_struct *cable, char **ev);
 void	ft_env_set(t_struct *cable);
