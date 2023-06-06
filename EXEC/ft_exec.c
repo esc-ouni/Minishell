93802df09363ab432d25c0b25604dcd9d56e9293
Mohamed-Taha Samhaoui <msamhaou@e2r6p10.1337.ma>
2023-06-06 17:02:29 +0100

    err sry

--- EXEC/ft_exec.c
+++ EXEC/ft_exec.c
@@ -6,7 +6,7 @@
 /*   By: msamhaou <msamhaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 18:36:07 by msamhaou          #+#    #+#             */
-/*   Updated: 2023/06/06 16:41:10 by msamhaou         ###   ########.fr       */
+/*   Updated: 2023/06/06 17:02:01 by msamhaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,15 +99,31 @@ int	ft_fork(t_cmd *cmd, t_struct *cable)
 	return (0);
 }
 
+int	ft_cmd_count(t_cmd *cmd)
+{
+	int	i = 0;
+
+	while (cmd)
+	{
+		i++;
+		cmd = cmd->next;
+	}
+	return (i);
+}
+
 void	ft_exec(t_struct *cable)
 {
 	t_cmd	*cmd;
+	int		numl;
 
 	cmd = cable->cmd;
 	ft_set_path(cmd, cable);
+	cable->cmd_numb = ft_cmd_count(cmd);
 	while (cmd)
 	{
 		ft_fork(cmd, cable);
 		cmd = cmd->next;
 	}
+	while (cable->cmd_numb--)
+		wait(NULL);
 }
