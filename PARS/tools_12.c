5133c6f967228d4c308124c3cbe46c900ac54cf4
esc-ouni <idouni@student.1337.ma>
2023-06-07 20:55:32 +0100

    SIG_IN_PRGS

--- PARS/tools_12.c
+++ PARS/tools_12.c
@@ -6,7 +6,7 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 17:09:45 by idouni            #+#    #+#             */
-/*   Updated: 2023/06/07 19:23:57 by idouni           ###   ########.fr       */
+/*   Updated: 2023/06/07 20:55:20 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ void sig_h(int sig)
 {
 	if (sig == SIGINT && g_var == 3)
 	{
-		write (1, "HEREDOC\n", 9);
+		write (1, "TADA\n", 9);
 		// rl_redisplay();
-		// // rl_on_new_line();
+		// rl_on_new_line();
 	}
 	else if (sig == SIGINT && g_var == 1)
 	{
