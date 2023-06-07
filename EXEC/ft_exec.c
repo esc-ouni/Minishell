e4984484aa9d227eb7a2f57165b69a157990e132
esc-ouni <idouni@student.1337.ma>
2023-06-07 19:29:07 +0100

    SIG_IN_PRGS

--- EXEC/ft_exec.c
+++ EXEC/ft_exec.c
@@ -6,7 +6,7 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 18:36:07 by msamhaou          #+#    #+#             */
-/*   Updated: 2023/06/07 19:26:41 by idouni           ###   ########.fr       */
+/*   Updated: 2023/06/07 19:28:50 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,7 +100,7 @@ int	ft_fork(t_cmd *cmd, t_struct *cable)
 	cmd->pipe_fd = fd;
 	pid = fork();
 	if (!ft_strncmp(cable->cmd->cmd[0], "./minishell", ft_strlen("./minishell")))
-		g_var += 4;
+		g_var += 2;
 	if (!pid)
 		ft_child(cmd, fd, cable);
 	else
