c794b0bcd1c4225ded6ece6e2595c44ffafa7eb4
esc-ouni <idouni@student.1337.ma>
2023-06-11 14:09:49 +0100

    TBC

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 14:42:08 by msamhaou          #+#    #+#             */
-/*   Updated: 2023/06/11 13:12:47 by idouni           ###   ########.fr       */
+/*   Updated: 2023/06/11 14:09:27 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ void	program(t_struct *cable)
 	if (dup2(cable->tmp_fd_in, 0) == -1)
 		return (perror(""), ft_collectorclear(cable->collector, ALL));
 	cable->cmd = get_cmd(cable);
-	after_parse2(cable->cmd);
+	// after_parse2(cable->cmd);
 	ft_exec(cable);
 	get_sig_exitval(cable, g_var);
 	g_var = 1;
