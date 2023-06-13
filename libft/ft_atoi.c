7707d768b6ec68b0a4b88e4e233107fd1dabc7dd
esc-ouni <idouni@student.1337.ma>
2023-06-13 22:56:43 +0100

    LONG_LONG_EXIT_VAL_FULY_DONE

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 15:47:14 by msamhaou          #+#    #+#             */
-/*   Updated: 2023/06/13 22:28:15 by idouni           ###   ########.fr       */
+/*   Updated: 2023/06/13 22:39:03 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 size_t	ft_atoi(const char *str)
 {
-	size_t	res;
-	int		sign;
+	size_t				res;
+	int					sign;
 
 	res = 0;
 	sign = 1;
@@ -30,14 +30,14 @@ size_t	ft_atoi(const char *str)
 		str++;
 	while (*str >= '0' && *str <= '9')
 	{
+		if (res == 922337203685477580 && sign == -1 && (*str - '0' == 8))
+			return (0);
 		res *= 10;
 		res += *str - '0';
 		if (res == 9223372036854775807 && sign == 1)
 			return (res % 256);
 		else if (res > 9223372036854775807 && (sign == 1 || sign == -1))
 			return (-1);
-		else if (res > 9223372036854775807 && sign == -1)
-			return (0);
 		str++;
 	}
 	return (res * sign);
